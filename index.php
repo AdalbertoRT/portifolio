da8413aa4d6a912a738bbc0c414636aaa850e51b
Adalberto R. Teixeira <adalbertoart@gmail.com>
2020-12-14 15:23:29 -0300

    Comecando media queries

--- index.php
+++ index.php
@@ -1,155 +1,127 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mqueries.css">
     <!--    <link rel="stylesheet" href="css/bootstrap.min.css"> -->
     <!-- <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> -->
     <title>Adalberto Rodrigues Teixeira</title>
 </head>
+
 <body>
     <!-- CAPA DO PORTIFOLIO -->
-    <div id="capa">
-        <section class="capa">
+    <header>
+        <nav class="sociais">
+            <ul>
+                <li class="facebook"><a href="https://www.facebook.com/adalbertoart" target="_blank"><img src="images/icons/facebook.png" alt="Facebook"></a></li>
+                <li class="instagram"><a href="https://www.instagram.com/adalbertoart/" target="_blank"><img src="images/icons/instagram.png" alt="Instagram"></a></li>
+                <li class="linkedin"><a href="linkedin.com/in/adalberto-teixeira" target="_blank"><img src="images/icons/linkedin.png" alt="Linkedin"></a></li>
+                <li class="whatsapp"><a href="https://api.whatsapp.com/send?phone=5517997033384&text=Olá!%20Meu%20nome%20é%20Adalberto.%20Sou%20Desenvolvedor%20Web.%20Em%20que%20posso%20ajudá-lo?" target="_blank"><img src="images/icons/whatsapp.png" alt="Whatsapp"></a></li>
+                <li class="github"><a href="https://github.com/AdalbertoRT" target="_blank"><img src="images/icons/github_icon.png" alt="Github"></a></li>
+                <li class="mail"><a href="mailto:adalbertoart@gmail.com?subject=Olá!%20Tudo%20bem?" target="_blank"><img src="images/icons/mail.png" alt="E-mail"></a></li>
+            </ul>
+        </nav>
+    </header>
+    <section id="capa">
+        <div class="capa">
             <!-- <div id="foto">
                 <div id="eu"></div>
             </div> -->
-        </section>
+        </div>
         <aside>
-            <div id="port"  class="scalable"><a href="">PORTIFÓLIO</a></div>
+            <div id="port" class="scalable"><a href="">PORTIFÓLIO</a></div>
             <div id="webDev">
                 <div id="web">
                     <span class="titulo">WEB</span>
                 </div>
-               <div id="nome_dev">
-                <div id="nome" class="scalable"><a href="">Adalberto Teixeira</a></div>
-                <span id="dev" class="titulo">DEVELOPER</span>
-               </div>
+                <div id="nome_dev">
+                    <div id="nome" class="scalable"><a href="#about">Adalberto Teixeira</a></div>
+                    <span id="dev" class="titulo">DEVELOPER</span>
+                </div>
             </div>
-            
-            <span id="ano"><?php echo date("Y")?></span>
+
+            <span id="ano"><?php echo date("Y") ?></span>
         </aside>
-    </div>
-    <!-- PAGINA 2 -->
-    <div id="page2">
-        <section>
-            <div id="about">
-                <h4>Quem sou eu? &nbsp;<small>(About)</small></h4>
-                <div id="aboutContent">
-                    <div id="bordaSup">
-                        <div id="et"></div>
-                        <div id="dt"></div>
-                    </div>                  
-                    
-                    <div id="imgInfo">
-                        <div id="eu2" title="Este sou eu!"></div>
-                        <div id="info">
-                            Meu nome é <strong class="destaque nome">Adalberto Rodrigues Teixeira</strong> e sou Desenvolvedor Web. Sou formado em <span class="destaque">Análise e Desenmvolvimento de Sistemas</span> e tenho como especialização, o desenvolvimento de aplicações web. Estou apto a codificar tanto o <span class="destaque">front-end</span> como o <span class="destaque">back-end</span>​, ou seja, <span class="destaque">sou desenvolvedor Full Stack</span>. A stack que mais domino é: PHP, HTML5, CSS3, Javascript e MySQL assim como, alguns frameworks e editores de imagem.<br/><br/>
-                            
-                            Minha graduação foi pela Unifran em modelo EAD, através do sistema de ensino Cruzeiro do Sul Virtual. Mas minhas experiências com programação vem  desde 2007 quando resolvi prestar vestibular para o curso de Informática para a Gestão de Negócios na Fatec - São José do Rio Preto - SP, cidade onde resido. Cursei este curso por 3 anos, mas precisei trancar por motivos pessoais. Fiz outros cursos posteriormente de <span class="destaque">Administração</span> e <span class="destaque">Design Gráfico</span>, mas retornei para a minha primeira paixão, a informática <img class="heart" src="images/heart-anil.png" alt="">.
-                            <br/><br/>
-    
-                            Em paralelo a programação sempre exerci a profissão de barbeiro (eu sei, não tem nada a ver com a área kkkk), que me deu combustível para que eu realizasse meus estudos. Depois de anos de estudo, hoje me sinto seguro e experiente para exercer a profissão de <span class="destaque">programador</span>.<br/><br/>
-    
-                            Hoje <span class="destaque">continuo meus estudos</span> com cursos online de altíssima qualidade, pela B7web, Udemy, Rocketseat, Curso em Video entre outros. Já realizei varios projetos que são aplicados nestes cursos, desde Landing Pages, Sites Institucionais, Lojas Virtuais, Portais, Portifólios e muitas outras aplicações. Além dos cursos participo de comunidades, canais de programadores, fóruns e <span class="destaque">estou conectado</span> a varios grupos de programadores (Whatsapp  e redes sociais). Assim me mantenho sempre atualizado.
-                        </div>
-                    </div>
-                    <div id="bordaInf">
-                        <div id="eb"></div>
-                        <div id="db"></div>
+    </section>
+
+    <!-- Sobre mim -->
+    <section id="page2">
+        <div id="about">
+            <h2>Quem sou eu? &nbsp;<small>(About)</small></h2>
+            <div id="aboutContent">
+                <div id="bordaSup">
+                    <div id="et"></div>
+                    <div id="dt"></div>
+                </div>
+
+                <div id="imgInfo">
+                    <div id="eu2" title="Este sou eu!"></div>
+                    <div id="info">
+                        Meu nome é <strong class="destaque nome">Adalberto Rodrigues Teixeira</strong> e sou <span class="destaque">Desenvolvedor Web</span>. Sou formado em <span class="destaque">Análise e Desenmvolvimento de Sistemas</span> e tenho como especialização, o desenvolvimento de aplicações web. Estou apto a codificar tanto o <span class="destaque">front-end</span> como o <span class="destaque">back-end</span>​, ou seja, <span class="destaque">sou desenvolvedor Full-Stack</span>. A stack que mais domino é: <span class="destaque">PHP, HTML5, CSS3, Javascript e MySQL</span> assim como, alguns <span class="destaque">frameworks de back e front-ends e editores de imagem</span>.<br /><br />
+
+                        Minha <span class="destaque">graduação</span> foi pela <span class="destaque">Unifran</span> em modelo EAD, através do sistema de ensino Cruzeiro do Sul Virtual. Mas minhas experiências com programação vem desde 2007 quando resolvi prestar vestibular para o curso de <span class="destaque">Informática para a Gestão de Negócios</span> na Fatec - São José do Rio Preto - SP, cidade onde resido. Fiz outros cursos posteriormente de <span class="destaque">Administração</span> e <span class="destaque">Design Gráfico</span>, mas retornei para a minha primeira paixão, a <span class="destaque">informática</span> <img class="heart" src="images/heart-anil.png" alt="">.
+                        <br /><br />
+
+                        <!-- Em paralelo a programação sempre exerci a profissão de barbeiro (eu sei, não tem nada a ver com a área kkkk), que me deu combustível para que eu realizasse meus estudos. Depois de anos de estudo, hoje me sinto seguro e experiente para exercer a profissão de <span class="destaque">programador</span>.<br /><br /> -->
+
+                        Hoje <span class="destaque">continuo meus estudos</span> com <span class="destaque">cursos online de altíssima qualidade</span>, pela <span class="destaque">B7web, Udemy, Origamid, Digital Innovation One, Curso em Video, entre outros</span>. Já realizei varios projetos que são aplicados nestes cursos, desde <span class="destaque">Landing Pages, Sites Institucionais, Lojas Virtuais, Portais, Portifólios e muitas outras aplicações</span>. Além dos cursos participo de comunidades, canais de programadores, fóruns e <span class="destaque">estou conectado</span> a varios grupos de programadores (Whatsapp e redes sociais). Assim me mantenho sempre atualizado.
                     </div>
                 </div>
-            </div>
-        </section>
-    </div>
-
-    <div id="page3">
-        <div id="skills">
-            <h4>Habilidades <small>(Skills)</small></h4>
-            <div id="slideSkills" >
-                    <div class="skill display">
-                        <fieldset>
-                            <legend>Front-end</legend>
-                            <div class="item">
-                                <span>&#9673; HTML5</span><div class="barra" nivel="80%"><div class="nivel"></div></div>
-                            </div>
-                            <div class="item">
-                                <span>&#9673; CSS3</span><div class="barra" nivel="70%"><div class="nivel"></div></div>
-                            </div>
-                            <div class="item">
-                                <span>&#9673; Javascript</span><div class="barra" nivel="50%"><div class="nivel"></div></div>
-                            </div>
-                        </fieldset>
-                    </div>
-                    <div class="skill">
-                        <fieldset>
-                            <legend>Back-end</legend>
-                            <div class="item">
-                                <span>&#9673; PHP</span><div class="barra" nivel="70%"><div class="nivel"></div></div>
-                            </div>
-                            <div class="item">
-                                <span>&#9673; Node.js</span><div class="barra" nivel="20%"><div class="nivel"></div></div>
-                            </div>
-                            <div class="item">
-                                <span>&#9673; Python</span><div class="barra" nivel="40%"><div class="nivel"></div></div>
-                            </div>
-                            <div class="item">
-                                <span>&#9673; MySQL</span><div class="barra" nivel="70%"><div class="nivel"></div></div>
-                            </div>
-                        </fieldset>
-                    </div>
-                    <div class="skill">
-                        <fieldset>
-                            <legend>Frameworks</legend>
-                            <div class="item">
-                                <span>&#9673; Laravel 6</span><div class="barra" nivel="50%"><div class="nivel"></div></div>
-                            </div>
-                            <div class="item">
-                                <span>&#9673; Bootstrap 4</span><div class="barra" nivel="80%"><div class="nivel"></div></div>
-                            </div>
-                            <div class="item">
-                                <span>&#9673; ReactJS</span><div class="barra" nivel="20%"><div class="nivel"></div></div>
-                            </div>
-                            <div class="item">
-                                <span>&#9673; Vue</span><div class="barra" nivel="20%"><div class="nivel"></div></div>
-                            </div>
-                        </fieldset>
-                    </div>
-                
-                <div class="controles">
-                    <a href="javascript:void(0);" class="setas prev" onclick="habilidade()">&#9756;</a>
-                    <a href="javascript:void(0);" class="setas next">&#9758;</a>
-                </div>
-                
-            </div>
-        </div>
-    </div>
-    <div id="page4">
-        <div id="cert">
-                <h4>Meus Certificados <small>(Certificates)</small></h4>
-                <div id="slideCert">
-                    <div class="certificados">
-
-                    </div>
-                    <div class="controles">
-                        <a href="#slideCert" class="setas prev" onclick="mudaCert(-1)">&#9756;</a>
-                        <a href="#slideCert" class="setas next" onclick="mudaCert(1)">&#9758;</a>
-                    </div>
-                    
+                <div id="bordaInf">
+                    <div id="eb"></div>
+                    <div id="db"></div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
-    <div id="page5">
-        <div>Meus Projetos <small>(Projects)</small></div>
-    </div>
+    <!-- Conhecimentos -->
+    <section id="page3">
+        <div id="knowledges">
+            <h2>Conhecimentos &nbsp;<small>(Knowledge)</small></h2>
+            <div id="knowledge">
+                <!-- <ul class="knows">
+                    <h3 class="know-title">--</h3>
+                    <li><img src=""><span>--</span></li>
+                </ul> -->
+            </div>
+        </div>
 
+    </section>
+
+    <!-- Projetos -->
+    <section id="page4">
+        <h2>Meus Projetos <small>(Projects)</small></h2>
+        <div id="projects">
+            <!-- <div class="card">
+                <div class="card-body">
+
+                </div>
+                <div class="card-text"></div>
+            </div> -->
+        </div>
+    </section>
+
+    <footer>
+        <p>Adalberto Rodrigues Teixeira - <?php echo Date('Y'); ?></p>
+        <nav class="sociais">
+            <ul>
+                <li class="facebook"><a href="https://www.facebook.com/adalbertoart" target="_blank"><img src="images/icons/facebook.png" alt="Facebook"></a></li>
+                <li class="instagram"><a href="https://www.instagram.com/adalbertoart/" target="_blank"><img src="images/icons/instagram.png" alt="Instagram"></a></li>
+                <li class="linkedin"><a href="linkedin.com/in/adalberto-teixeira" target="_blank"><img src="images/icons/linkedin.png" alt="Linkedin"></a></li>
+                <li class="whatsapp"><a href="https://api.whatsapp.com/send?phone=5517997033384&text=Olá!%20Meu%20nome%20é%20Adalberto.%20Sou%20Desenvolvedor%20Web.%20Em%20que%20posso%20ajudá-lo?" target="_blank"><img src="images/icons/whatsapp.png" alt="Whatsapp"></a></li>
+                <li class="github"><a href="https://github.com/AdalbertoRT" target="_blank"><img src="images/icons/github_icon.png" alt="Github"></a></li>
+                <li class="mail"><a href="mailto:adalbertoart@gmail.com?subject=Olá!%20Tudo%20bem?" target="_blank"><img src="images/icons/mail.png" alt="E-mail"></a></li>
+            </ul>
+        </nav>
+        <button><a href="images/curriculum.pdf" download="Curriculum.pdf"><img src="images/icons/curriculo.png" alt="Curriculo">Baixar Currículum</a></button>
+    </footer>
+
+    <!-- <script type="text/javascript" src="js/knowledges.js"></script> -->
+    <script type="text/javascript" src="js/script.js"></script>
 </body>
 
-<!-- <script src="js/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script> -->
-<script src="js/script.js"></script>
-<!-- <script src="js/vue.js"></script> -->
 </html>
--- js/script.js
+++ js/script.js
@@ -1,104 +1,186 @@
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
 
-// let app = new Vue({
-//     el:'#capa',
-//     data: {
-//         year: new Date().getFullYear(),
-//     }
-// });
+//CONHECIMENTOS
 
-// SKILLS
-var skill = document.querySelectorAll('.skill');
-var sliderSkill, skillAtual, totalSkill, time;
-var barras = document.querySelectorAll(".barra");
-var niveis = document.querySelectorAll(".nivel");
+function initKnowledges() {
+  async function knowJson() {
+    const response = await fetch("./js/knowledges.json");
+    const json = await response.json();
+    knows = json.knowJson;
 
-function mostrarSkill(s){
-    skill[s].classList.add("display");
-    barra();
+    knows.map((item, index) => {
+      // let knows = c('.knows');
+      let knowledge = c("#knowledge");
+      let ul = document.createElement("ul");
+      ul.classList.add("knows");
+      let h3 = document.createElement("h3");
+      h3.classList.add("know-title");
+      h3.innerHTML = item.titulo;
+      ul.appendChild(h3);
+      knowledge.append(ul);
+      item.ferramentas.forEach((element) => {
+        let li = document.createElement("li");
+        let img = document.createElement("img");
+        let span = document.createElement("span");
+        (img.src =
+          "images/icons/" + element.toLowerCase().replace(" ", "") + ".png"),
+          (span.innerHTML = element);
+        li.append(img, span);
+
+        ul.appendChild(li);
+      });
+    });
+  }
+  knowJson();
 }
+initKnowledges();
 
-function iniciarSkills(){
-    skillAtual = 0;
-    totalSkill = skill.length-1;
-    sliderSkill = document.querySelector(".skills");
-    mostrarSkill(skillAtual);
-    time = setInterval(mudaSkill, 6000);
-    
-}
-
-function animaScroll() {
-  console.log("Animou");
-}
-
-function barra(){
-    for(let i = 0; i < barras.length; i++){
-        niveis[i].style.width = barras[i].getAttribute("nivel");
+function initAnimaScroll() {
+  function animaScroll() {
+    const knows = cs(".knows");
+    if (knows.length) {
+      const windowMetade = window.innerHeight * 0.8;
+      knows.forEach((el) => {
+        const knowsTop = el.getBoundingClientRect().top;
+        const knowVisible = knowsTop - windowMetade < 0;
+        if (knowVisible) {
+          el.classList.add("animar");
+        }
+      });
     }
+  }
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
+initAnimaScroll();
+
+function myProjects() {
+  const projects = c("#projects");
+
+  async function project() {
+    const response = await fetch("./js/projects.json");
+    const json = await response.json();
+
+    json.projects.map((item) => {
+      let card = document.createElement("div");
+      card.classList.add("card");
+      let cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+      cardBody.setAttribute("data-info1", item.info[0]);
+      cardBody.setAttribute("data-info2", item.info[1]);
+      let thumb = document.createElement("img");
+      thumb.classList.add("thumb");
+      thumb.src = `${"./images/projetos/" + item.thumb}`;
+      thumb.setAttribute("data-image", item.image);
+      cardBody.append(thumb);
+      let cardText = document.createElement("div");
+      cardText.classList.add("card-text");
+      let h4 = document.createElement("h4");
+      h4.innerText = item.name;
+      let p1 = document.createElement("p");
+      p1.innerText = item.type;
+      let tech = document.createElement("div");
+      tech.classList.add("tecnology");
+      item.tecnology.map((i) => {
+        let icon = document.createElement("img");
+        icon.src = "./images/icons/" + i + ".png";
+        icon.title = i.toUpperCase();
+        icon.alt = i.toUpperCase();
+        tech.appendChild(icon);
+      });
+      cardText.append(h4, p1, tech);
+
+      card.append(cardBody, cardText);
+
+      projects.append(card);
+    });
+    initModalProjeto();
+  }
+  project();
+}
+myProjects();
+
+function initModalProjeto() {
+  const projeto = cs(".card");
+  projeto.forEach((item) => {
+    item.addEventListener("click", () => {
+      let src = `${
+        document.location.origin +
+        document.location.pathname +
+        "images/projetos/" +
+        item.querySelector(".thumb").getAttribute("data-image")
+      }`;
+
+      let modal = document.createElement("div");
+      modal.classList.add("modal", "background");
+      let modalContainer = document.createElement("div");
+      modalContainer.classList.add("modalContainer");
+      let btnContainer = document.createElement("div");
+      btnContainer.classList.add("btnContainer");
+      let fechar = document.createElement("button");
+      fechar.innerText = "x";
+      fechar.classList.add("fechar");
+      let info = document.createElement("button");
+      info.innerText = "Info";
+      info.classList.add("info");
+      btnContainer.append(fechar, info);
+      let modalContent = document.createElement("div");
+      modalContent.classList.add("modalContent");
+      let img = document.createElement("img");
+      img.src = src;
+      modalContent.append(img);
+      modalContainer.append(btnContainer, modalContent);
+      modal.append(modalContainer);
+
+      c("body").append(modal);
+      c("html").style.overflowY = "hidden";
+      fechaModal();
+
+      let info1 = item.querySelector(".card-body").getAttribute("data-info1");
+      let info2 = item.querySelector(".card-body").getAttribute("data-info2");
+      initModalInfo(info1, info2);
+    });
+  });
 }
 
-// CERTIFICADOS
-var imgs=[];
-var slider;
-var imgAtual;
-var totalImgs;
-var tmp;
-
-function preCarregamento(){
-    for(let i = 0; i < 13; i++){
-        imgs[i] = new Image();
-        imgs[i].src = "./images/certificados/certificado"+i+".png";
+function fechaModal() {
+  let modal = c(".modal");
+  modal.addEventListener("click", ({ target }) => {
+    if (
+      target.classList.contains("modal") ||
+      target.classList.contains("fechar")
+    ) {
+      modal.remove();
+      c("html").style.overflowY = "auto";
     }
+  });
 }
 
-function carregarImg(img){
-    slider.style.backgroundImage = "url('"+imgs[img].src+"')";
+function initModalInfo(info1, info2) {
+  let modal = document.createElement("div");
+  modal.classList.add("modalInfo");
+  let fechar = document.createElement("button");
+  fechar.innerText = "x";
+  fechar.classList.add("fechar");
+  let h4 = document.createElement("h4");
+  h4.innerText = info1;
+  let p = document.createElement("p");
+  p.innerText = info2;
+  modal.append(fechar, h4, p);
+  c(".modalContainer").appendChild(modal);
+  mostrarInfo();
 }
 
-function iniciar(){
-    preCarregamento();
-    imgAtual = 0;
-    totalImgs = imgs.length-1;
-    slider = document.querySelector(".certificados");
-    carregarImg(imgAtual);
-    tmp = setInterval(mudaCert, 3000);
-}
+function mostrarInfo() {
+  let info = c(".info");
+  let modalInfo = c(".modalInfo");
 
-function mudaCert(ir = 1){
-    imgAtual += ir;
-    if(imgAtual > totalImgs){
-        imgAtual = 0;
-    }
-    if(imgAtual < 0){
-        imgAtual = totalImgs;
-    }
-    carregarImg(imgAtual);    
+  if (info) {
+    info.addEventListener("click", () => {
+      modalInfo.classList.toggle("ativo");
+      c(".modalContent").style.opacity = "0.3";
+    });
+  }
 }
-window.addEventListener('load', iniciar);
-
-// function includeHTML() {
-//   var z, i, elmnt, file, xhttp;
-//   /* Loop through a collection of all HTML elements: */
-//   z = document.getElementsByTagName("*");
-//   for (i = 0; i < z.length; i++) {
-//     elmnt = z[i];
-//     /*search for elements with a certain atrribute:*/
-//     file = elmnt.getAttribute("w3-include-html");
-//     if (file) {
-//       /* Make an HTTP request using the attribute value as the file name: */
-//       xhttp = new XMLHttpRequest();
-//       xhttp.onreadystatechange = function() {
-//         if (this.readyState == 4) {
-//           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-//           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-//           /* Remove the attribute, and call this function once more: */
-//           elmnt.removeAttribute("w3-include-html");
-//           includeHTML();
-//         }
-//       }
-//       xhttp.open("GET", file, true);
-//       xhttp.send();
-//       /* Exit the function: */
-//       return;
-//     }
-//   }
-// }
